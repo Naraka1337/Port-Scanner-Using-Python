e74ac1b80f3a7e55bd6ae840f2a8eaa1b540e1ee
Omar Alaa <168812089+Naraka1337@users.noreply.github.com>
2024-09-21 18:40:59 +0200

    PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -29,21 +29,31 @@ def scan_ports(targetIP, start_port, end_port):
 
 if __name__ == "__main__":
     print("_NARAKA PORT SCANNER_\n")
-    targetIP = input("Target IP: ")
-
-    # The Banner
-    print("_" * 50)
-    print("Scanning Target: " + targetIP)
-    print("Scanning Started at: " + str(datetime.now()))
-    print("_" * 50)
-
+    
     try:
-        # Allow the user to specify the port range
+        targetIP = input("Target IP: ")
+        # Validate the IP address
+        socket.inet_aton(targetIP)
+
+        # The Banner
+        print("_" * 50)
+        print("Scanning Target: " + targetIP)
+        print("Scanning Started at: " + str(datetime.now()))
+        print("_" * 50)
+
+        # Allow the user to specify the port range and validate inputs
         start_port = int(input("Enter the start port: "))
         end_port = int(input("Enter the end port: "))
+        
+        if start_port < 1 or end_port > 65535 or start_port > end_port:
+            raise ValueError("Invalid port range specified. Please enter a valid range between 1 and 65535.")
 
         # Start multithreading scan
         scan_ports(targetIP, start_port, end_port)
+
+    except ValueError as ve:
+        print(f"\nError: {ve}")
+        sys.exit()
 
     except KeyboardInterrupt:
         print("\n Exiting... Bye Bye!")
